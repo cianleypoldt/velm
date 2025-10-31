b2678f3669a07ad3e4c79e666d65e55db57e3f36
Cian Leypoldt <cianleypoldt@gmail.com>
2025-10-31 19:28:07 +0100

    shaders compile

---
+++ shaders/common.sh
@@ -0,0 +1,6 @@
+
+// Use local bgfx shader header (copied into velm/shaders/)
+#include "bgfx_shader.sh"
+
+// Include shaderlib from bgfx examples (keeps original shader library intact) */
+#include "../external/bgfx.cmake/bgfx/examples/common/shaderlib.sh"
